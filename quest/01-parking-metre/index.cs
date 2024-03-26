454848c1031dd0be61c10beaf66e39447939eca2
Gilles-Z <164996918+Gilles-Z@users.noreply.github.com>
2024-03-26 22:38:05 +0800

    Under guidance

--- quest/01-parking-metre/index.cs
+++ quest/01-parking-metre/index.cs
@@ -9,36 +9,45 @@ using System.Threading.Tasks;
 namespace HELLO_WORLD {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static string LeftPad(string text, int length, string padToken) {
+            if (padToken.Length > 1) {
+                throw new ArgumentException("padToken can only accept a single character");
+            };
+            StringBuilder result = new StringBuilder("", length);
+            int beforeFill = length - text.Length;
+            while (result.Length < beforeFill) {
+                result.Append(padToken);
+            }
+            result.Append(text);
+            return result.ToString();
+        }
+        static void Main(string[] args) {
             float[] n = new float[3];
-            for (int i = 0; i < 3; i++)
-            {
+            for (int i = 0; i < 3; i++) {
                 float num = 0;
                 num = float.Parse(Console.ReadLine());
                 n[i] = num;
             }
-            foreach (float num in n)
-            {
+            Console.WriteLine("Customer   Hours   Charge");
+            for (int i = 0; i < n.Length; i ++) {
+                float num = n[i];
+                Console.Write(LeftPad((i + 1).ToString(), 8, " "));
+                Console.Write(LeftPad(num.ToString("n2"), 8, " "));
                 byte money = 0;
-                if (num <= 3)
-                {
+                if (num <= 3) {
                     money = 2;
                 }
-                else if (num < 19)
-                {
+                else if (num < 19) {
                     money = (byte)Math.Ceiling((num + 1) / 2);
                 }
-                else if (num <= 24)
-                {
+                else if (num <= 24) {
                     money = 10;
                 }
-                else
-                {
-                    Console.WriteLine("Fuck off");
+                else {
+                    Console.Write("    Fuck!");
                     continue;
                 };
-                Console.WriteLine(money);
+                Console.WriteLine(LeftPad(money.ToString("n2"), 9, " "));
             }
             Console.ReadKey();
         }
