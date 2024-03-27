ca5971cbeba48b934c323bbe29d8dbc1e3356a1e
Gilles-Z <164996918+Gilles-Z@users.noreply.github.com>
2024-03-27 17:25:17 +0800

    Update index.cs

--- quest/02-hollow-square/index.cs
+++ quest/02-hollow-square/index.cs
@@ -1,1 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace HELLO_WORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            num = int.Parse(Console.ReadLine());
+            int n = 0; int m = 0;
+            while (n < num)
+            {
+                Console.Write("@");
+                n++;
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < (num - 2); i++)
+            {
+                Console.Write("@");
+                for (int k = 0; k < (num - 2); k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("@");
+                Console.WriteLine("");
+
+
+
+            }
+            while (m < num)
+            {
+                Console.Write("@");
+                m++;
+
+
+               
+            }
+            Console.ReadKey();
+        } 
+
+
+
+    }
+}
