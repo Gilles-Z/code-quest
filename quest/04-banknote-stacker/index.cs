b2c1ab45b4b02d9aa230d495642fae49035d81b3
Gilles-Z <164996918+Gilles-Z@users.noreply.github.com>
2024-03-31 16:05:17 +0800

    Update index.cs

--- quest/04-banknote-stacker/index.cs
+++ quest/04-banknote-stacker/index.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace ArrayProps
 {
@@ -41,26 +42,42 @@ namespace ArrayProps
             }
             return Employee;
          }
+        static string Fill(string text,int length,string Padtoken) 
+        {
+            StringBuilder resulat = new StringBuilder("", length);
+            int beforeFille = length - text.Length;
+
+            while (beforeFille > resulat.Length)
+            {
+                resulat.Append(Padtoken);
+            }
+            
+            resulat.Append(text);
+          
+            return resulat.ToString();
+        }
 
         static void Main(string[] args)
         {
-            int[] banknote1 = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+            
+            int[] banknote1 = { 500, 200, 100, 50, 20,  10, 5, 2, 1};
             int[] number = new int[5];
             int[, ] result = new int[5, 9];
             EmployeeFill(number);
+            Console.WriteLine("Salary  500  200  100   50   20   10    5    2    1");
             for (int y = 0; y < 5; y++)
-            {
+            {   
                 for (int i = 0; i < 9; i++)
                 {
                     result[y, i] = Split(number[y], banknote1)[i];
                 }
             };
               
-            for (int y1 = 0; y1 < 5;y1++) { 
-
+            for (int y1 = 0; y1 < 5;y1++) {
+                Console.Write(Fill(number[y1].ToString(),6," "));
                 for (int i2 = 0;i2 < 9; i2++) {
-                    Console.Write(result[y1,i2]);
-                    Console.Write(" ");
+                    Console.Write(Fill((result[y1,i2]).ToString(),5," "));
+                    
                 }
                Console.WriteLine("");
             }
